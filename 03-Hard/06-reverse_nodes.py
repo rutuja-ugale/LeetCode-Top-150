df94a886f106a456d6e059ca1b3af3c97c3198db
rutujaa121 <rutujaaugale@gmail.com>
2026-08-03 21:41:47 +0530

    Reverse Nodes in K-Group

---
+++ 03-Hard/06-reverse_nodes.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: Optional[ListNode]
+        :type k: int
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+
+        while True:
+            # 1. Find the k-th node from groupPrev
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break
+            
+            groupNext = kth.next
+
+            # 2. Reverse the k nodes
+            prev, curr = groupNext, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+
+            # 3. Update pointers to reconnect the reversed segment
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+
+        return dummy.next
+
+    def getKth(self, curr, k):
+        """Helper to get the k-th node ahead of curr."""
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
