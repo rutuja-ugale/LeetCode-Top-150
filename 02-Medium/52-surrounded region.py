f8c4ae821ece6d6d5cc889b8d048474e598d47f5
rutujaa121 <rutujaaugale@gmail.com>
2026-09-02 10:14:43 +0530

    Surrounded Region

---
+++ 02-Medium/52-surrounded region.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        if not board or not board[0]:
+            return
+        
+        rows, cols = len(board), len(board[0])
+        
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != 'O':
+                return
+            # Temporarily mark boundary-connected 'O's
+            board[r][c] = 'T'
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+            
+        # 1. Check first and last columns for 'O's
+        for r in range(rows):
+            if board[r][0] == 'O':
+                dfs(r, 0)
+            if board[r][cols - 1] == 'O':
+                dfs(r, cols - 1)
+                
+        # 2. Check first and last rows for 'O's
+        for c in range(cols):
+            if board[0][c] == 'O':
+                dfs(0, c)
+            if board[rows - 1][c] == 'O':
+                dfs(rows - 1, c)
+                
+        # 3. Capture surrounded 'O's and restore safe 'T's
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'T':
+                    board[r][c] = 'O'
