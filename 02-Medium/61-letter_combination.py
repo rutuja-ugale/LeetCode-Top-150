53c5df505af443c47fc836f7e1eba84f71b3a59c
rutujaa121 <rutujaaugale@gmail.com>
2026-09-13 13:14:21 +0530

    Letter Combinations of a Phone Number add code

---
+++ 02-Medium/61-letter_combination.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+
+        phone_map = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        result = []
+
+        def backtrack(index, current_path):
+            if index == len(digits):
+                result.append("".join(current_path))
+                return
+
+            possible_letters = phone_map[digits[index]]
+            for letter in possible_letters:
+                current_path.append(letter)
+                backtrack(index + 1, current_path)
+                current_path.pop()
+
+        backtrack(0, [])
+        return result
