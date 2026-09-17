f9adba56f98a545345dc667e328944e12f830071
rutujaa121 <rutujaaugale@gmail.com>
2026-09-17 16:09:01 +0530

    N-Queens II

---
+++ 03-Hard/10-n_queens.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        cols = set()
+        pos_diag = set()  # (r + c)
+        neg_diag = set()  # (r - c)
+        
+        def backtrack(row):
+            if row == n:
+                return 1
+            
+            count = 0
+            for col in range(n):
+                if col in cols or (row + col) in pos_diag or (row - col) in neg_diag:
+                    continue
+                
+                # Place queen
+                cols.add(col)
+                pos_diag.add(row + col)
+                neg_diag.add(row - col)
+                
+                # Recurse to next row
+                count += backtrack(row + 1)
+                
+                # Remove queen (backtrack)
+                cols.remove(col)
+                pos_diag.remove(row + col)
+                neg_diag.remove(row - col)
+                
+            return count
+
+        return backtrack(0)
